0d8c6f6b4d5769685730d675263258dda3169fa3
Pacifisto092 <a.duvanov092@gmail.com>
2024-12-13 22:24:29 +0300

    add const

--- apiProjectt/utils/api.py
+++ apiProjectt/utils/api.py
@@ -4,11 +4,11 @@ from utils.json import *
 """Методы для тестирования Google_maps_api"""
 
 BASE_URL = "https://rahulshettyacademy.com"              # Базовая URL
-key = "?key=qaclick123"                                  # Параметр для всех запросов
-post_resource = "/maps/api/place/add/json"               # Ресурс метода POST
-get_resourse = "/maps/api/place/get/json"                # Ресурс метода GET
-put_resourse = "/maps/api/place/update/json"             # Ресурс метода PUT
-delete_resourecese = "/maps/api/place/delete/json"       # Ресурс метода DELETE
+KEY = "?key=qaclick123"                                  # Параметр для всех запросов
+POST_RESOURSE = "/maps/api/place/add/json"               # Ресурс метода POST
+GET_RESOURSE = "/maps/api/place/get/json"                # Ресурс метода GET
+PUT_RESOURSE = "/maps/api/place/update/json"             # Ресурс метода PUT
+DELETE_RESOURSE = "/maps/api/place/delete/json"       # Ресурс метода DELETE
 
 class GoogleMapsApi:
 
@@ -17,7 +17,7 @@ class GoogleMapsApi:
     def create_new_place():
         """Метод для созданияя новой локации"""
 
-        post_url = BASE_URL + post_resource + key
+        post_url = BASE_URL + POST_RESOURSE + KEY
         print(post_url)
         result_post = HttpMethods.post(post_url, json_for_create_new_place)
         print(result_post.text)
@@ -28,7 +28,7 @@ class GoogleMapsApi:
     def get_new_place(place_id):
         """Метод для проверки новой локации"""
 
-        get_url = BASE_URL + get_resourse + key + "&place_id={}".format(place_id)
+        get_url = BASE_URL + GET_RESOURSE + KEY + "&place_id={}".format(place_id)
         print(get_url)
         result_get = HttpMethods.get(get_url)
         print(result_get.text)
@@ -39,7 +39,7 @@ class GoogleMapsApi:
     def put_new_place(place_id):
         """Метод для изменения локации"""
 
-        put_url = BASE_URL + put_resourse + key
+        put_url = BASE_URL + PUT_RESOURSE + KEY
         print(put_url)
         json_for_update_place["place_id"] = place_id
         result_put = HttpMethods.put(put_url, json_for_update_place)
@@ -51,7 +51,7 @@ class GoogleMapsApi:
     def delete_new_place(place_id):
         """Метод удаления локации"""
 
-        delete_url = BASE_URL + delete_resourecese + key
+        delete_url = BASE_URL + DELETE_RESOURSE + KEY
         print(delete_url)
         json_for_delete_place["place_id"] = place_id
         result_delete = HttpMethods.delete(delete_url, json_for_delete_place)
